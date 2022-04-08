dbd6143a752d7c862a12815e87f404bf6ff02c2e
f12dq <f12dq@mail.ru>
2022-04-08 12:17:24 +0100

    server developement 1

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-console.log('hello');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+app.listen(port, () => console.log(`server is running ${port}`))
